eb5c092a339fd32f5e2b87536828f3d33a8b1f43
Zoia Apostoloska <zoiaapostoloska@gmail.com>
2021-02-07 14:11:49 +0200

    добавляет доп задания

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ const galleryRef = document.querySelector('.gallery');
 const lightBoxRef = document.querySelector('.lightbox');
 const bigImageRef = document.querySelector('.lightbox__image');
 
+let index;
+let imgIndex;
+
+
 images.map(image => {
     const galleryItem = document.createElement('li');
     galleryItem.classList.add('gallery__item');
@@ -14,7 +18,11 @@ images.map(image => {
     galleryImage.classList.add('gallery__image');
     galleryImage.setAttribute('src', image.preview);
     galleryImage.setAttribute('data-source', image.original);
-    galleryImage.setAttribute('alt', image.description)
+    galleryImage.setAttribute('alt', image.description);
+    index = images.indexOf(image)
+    galleryImage.setAttribute('data-index', index);
+
+
     galleryLink.append(galleryImage);
     galleryItem.append(galleryLink);
     galleryRef.append(galleryItem);
@@ -28,10 +36,36 @@ galleryRef.addEventListener('click', event => {
     const imgAlt = event.target.alt;
     bigImageRef.setAttribute('src', imgLink);
     bigImageRef.setAttribute('alt', imgAlt);
+    imgIndex = event.target.dataset.index;
 })
 
 lightBoxRef.addEventListener('click', event => {
     if(!event.target.classList.contains('lightbox__button') && !event.target.classList.contains('lightbox__overlay')) return;
-    lightBoxRef.classList.remove('is-open');
+    lightBoxRef.classList.remove('is-open'); //вынести в функци
     bigImageRef.setAttribute('src', '');
 })
+
+window.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+    lightBoxRef.classList.remove('is-open');
+    bigImageRef.setAttribute('src', '');
+    }
+    // if (KeyboardEvent.key='')
+})
+
+window.addEventListener('keydown', event => {
+
+    if ( event.key === 'ArrowLeft' && imgIndex > 0) {
+        imgIndex -= 1;
+        bigImageRef.setAttribute('src', images[imgIndex].original)
+    } else if (event.key === 'ArrowRight' && imgIndex < 10) {
+        imgIndex += 1;
+        bigImageRef.setAttribute('src', images[imgIndex].original)
+    } 
+
+    
+})
+
+
+
+// lightBoxRef.classList.contains('is-open') &&
